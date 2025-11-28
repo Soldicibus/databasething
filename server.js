56b4d249c3b742d82006ae093ca3efdba1b4dff1
Soldicibus <solobon2103@gmail.com>
2025-11-28 02:12:36 +0200

    Minor changes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express'); //Рядок 1
- const app = express(); //Рядок 2
- const port = process.env.PORT || 5000; //Рядок 3
+const express = require('express');
 
- // Повідомлення про те, що сервер запущений і прослуховує зазначений порт 
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Рядок 6
+const app = express();
+const PORT = process.env.PORT ?? 5000;
 
- // Створення GET маршруту
- app.get('/', (req, res) => { //Рядок 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Рядок 10
- }); //Рядок 11
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Express backend' });
+});
